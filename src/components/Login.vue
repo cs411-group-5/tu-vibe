b6646f015fd88e5399bc8e301cad47025f06a0e6
Zui <zuikng@gmail.com>
2019-03-24 15:21:11 -0400

    Update FB Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,48 @@
 <template>
+  <div>
+    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+    <button v-on:click="fbLogout">Logout</button>
     <div>
-        Hello
+        <h1>Facebook data</h1>
+        <p>{{fbAuthResp}}</p>
     </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+    data () {
+        return {
+            fbAuthResp: {}
+        };
+    },
+  methods: {
+    statusChangeCallback(r) {
+      if (r.status === "connected"){
+          this.fbAuthResp = r.authResponse;
+      } else {
+          this.fbAuthResp = {"error": r.status};
+      }
+    },
+    fbLogout (e) {
+        console.log("Logging out");
+        window.FB.logout( (r) => {
+            console.log(r);
+        });
+    },
+    checkLoginState() {
+      window.FB.getLoginStatus((response) => {
+        this.statusChangeCallback(response);
+      });
+    }
+  },
+  mounted() {
+    window.checkLoginState = this.checkLoginState;
+    this.checkLoginState();
+  }
+};
 </script>
 
 <style>
-
 </style>
