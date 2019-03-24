1418046bc7cf4f2dd3584ec8833931e579af4fb8
Zui <zuikng@gmail.com>
2019-03-24 15:28:53 -0400

    Add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import BootstrapVue from 'bootstrap-vue'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
