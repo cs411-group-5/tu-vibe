7471b7d252e76f727f02de6bcacda59cc1aadd5a
Zui <zuikng@gmail.com>
2019-03-24 14:35:39 -0400

    Get dist subtree

---
+++ dist/precache-manifest.ed611de979f63438ac4f9289a712e260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89be0ebfcb2760c06c9",
+    "url": "/js/about.1383eeaa.js"
+  },
+  {
+    "revision": "9c3d330d7aa75083b890",
+    "url": "/css/app.85d979d0.css"
+  },
+  {
+    "revision": "9c3d330d7aa75083b890",
+    "url": "/js/app.15f999ff.js"
+  },
+  {
+    "revision": "925011a2a50fe66b6c35",
+    "url": "/js/chunk-vendors.f5ca5aa6.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bbba46f40928639ed7647256997b480f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
