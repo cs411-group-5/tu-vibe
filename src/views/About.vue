91f3268260526071dd393f8e593011ad592da95f
Zui <zuikng@gmail.com>
2019-03-28 19:58:03 -0400

    About yelp

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{yelpJSON}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {yelpJSON: undefined};
+  },
+  methods: {
+    getYelp () {
+      fetch("https://cs411-group-5.github.io/web/example.json").then((r) => r.json()).then((j) => {
+        this.yelpJSON = j;
+      });
+    }
+  },
+  mounted() {
+    this.getYelp();
+  },
+}
+</script>
