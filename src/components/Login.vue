a477c6a9c894a067a037c36256a0ace11252c8ee
Zui <zuikng@gmail.com>
2019-03-28 20:17:05 -0400

    Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,38 +1,39 @@
 <template>
   <div>
-    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
-    <button v-on:click="fbLogout">Logout</button>
-    <div>
-        <h1>Facebook data</h1>
-        <p>{{fbAuthResp}}</p>
-    </div>
-
+    <b-card>
+      <h1>Facebook data</h1>
+      <div>
+        <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+        <b-button v-on:click="fbLogout">Logout</b-button>
+      </div>
+      <p>{{fbAuthResp}}</p>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            fbAuthResp: {}
-        };
-    },
+  data() {
+    return {
+      fbAuthResp: {}
+    };
+  },
   methods: {
     statusChangeCallback(r) {
-      if (r.status === "connected"){
-          this.fbAuthResp = r.authResponse;
+      if (r.status === "connected") {
+        this.fbAuthResp = r.authResponse;
       } else {
-          this.fbAuthResp = {"error": r.status};
+        this.fbAuthResp = { error: r.status };
       }
     },
-    fbLogout (e) {
-        console.log("Logging out");
-        window.FB.logout( (r) => {
-            console.log(r);
-        });
+    fbLogout() {
+      console.log("Logging out");
+      window.FB.logout(r => {
+        console.log(r);
+      });
     },
     checkLoginState() {
-      window.FB.getLoginStatus((response) => {
+      window.FB.getLoginStatus(response => {
         this.statusChangeCallback(response);
       });
     }
