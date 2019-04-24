71428c49aac9dfe80581afbab55b9c573cff13ae
Zui <zuikng@gmail.com>
2019-04-23 20:12:35 -0400

    Yelp!!!

--- api/routes/yelp.js
+++ api/routes/yelp.js
@@ -1,35 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const request = require("request");
 
 const YELP_CID = process.env.YELP_CID;
 const YELP_KEY = process.env.YELP_KEY;
 
+router.get("/", function(req, res, next) {
+    let rText = req.query.text;
 
-
-router.get('/', function(req, res, next) {
-
-  let rText = req.query.text;
-
-
-  let resOpts = {
-    url: "https://api.yelp.com/v3/businesses/search",
-    headers: {"Authorization":`Bearer ${YELP_KEY}`},
-    json: true,
-    qs: {
-      "latitude": "42.361145",
-      "longitude": "-71.057083",
-      "term": rText
-    }
-  };
-  console.log("Getting ", resOpts);
-  request.get(resOpts, function (error, response, body) {
-    res.json(body);
-  });
-
-
-
+    let resOpts = {
+        url: "https://api.yelp.com/v3/businesses/search",
+        headers: { Authorization: `Bearer ${YELP_KEY}` },
+        json: true,
+        qs: {
+            latitude: "42.361145",
+            longitude: "-71.057083",
+            term: rText
+        }
+    };
+    console.log("Getting ", resOpts);
+    request.get(resOpts, function(error, response, body) {
+        res.json(body);
+    });
 });
 
+router.get("/info", function(req, res, next) {
+    let id = req.query.id;
+
+    let resOpts = {
+        url: `https://api.yelp.com/v3/businesses/${id}`,
+        headers: { Authorization: `Bearer ${YELP_KEY}` },
+        json: true
+    };
+    console.log("Getting ", resOpts);
+    request.get(resOpts, function(error, response, body) {
+        res.json(body);
+    });
+});
 
 module.exports = router;
--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -1,20 +1,35 @@
 <template>
-    <div class="discover">
-        <title>Discover</title>
+  <div class="discover">
+    <title>Discover</title>
 
-        <!-- <Login /> -->
-        <h1>Discover this place</h1>  <!-- this location  -->
-        <Maps />
-
-    </div>
+    <!-- <Login /> -->
+    <h1>Discover this place</h1>
+    <button @click="getYelp()">Yelp!!</button>
+    <!-- this location  -->
+    <Maps/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'discover'
+export default {
+  name: "discover",
+  data() {
+    return { yelpJSON: undefined };
+  },
+  methods: {
+    getYelp() {
+      fetch(`http://localhost:8889/yelp/info/id=${this.id}`)
+        .then(r => r.json())
+        .then(j => {
+          this.yelpJSON = j;
+        });
     }
+  },
+  mounted() {
+    this.getYelp();
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
